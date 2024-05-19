ef30b34450ec2719f7b3ed9130ff169c5001e3db
Igor Larionov <jedialar@gmail.com>
2024-05-19 20:27:40 +0300

    create model Client

--- main/models.py
+++ main/models.py
@@ -1,5 +1,25 @@
-from datetime import datetime, timedelta
+from datetime import datetime
+
 import pytz
 from django.db import models
 
+from config import settings
 
+NULLABLE = {'null': 'True', 'blank': 'True'}
+zone = pytz.timezone(settings.TIME_ZONE)
+current_datetime = datetime.now(zone)
+
+
+class Client(models.Model):
+    email = models.EmailField(unique=True, verbose_name='почта')
+    comment = models.CharField(max_length=100, verbose_name='комментарий', **NULLABLE)
+    owner = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, verbose_name='пользователь',
+                              **NULLABLE)
+
+    def __str__(self):
+        return f'{self.email}'
+
+    class Meta:
+        verbose_name = 'клиент'
+        verbose_name_plural = 'клиенты'
+        ordering = ('email',)
