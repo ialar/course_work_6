91d43107d704db8b84d4e9c74255e1103baa128c
Igor Larionov <jedialar@gmail.com>
2024-05-19 21:24:31 +0300

    create model User

--- users/models.py
+++ users/models.py
@@ -9,7 +9,7 @@ class User(AbstractUser):
     email = models.EmailField(unique=True, verbose_name='почта')
     phone = models.CharField(max_length=35, verbose_name='телефон', **NULLABLE)
     country = models.CharField(max_length=100, verbose_name='страна', **NULLABLE)
-    avatar = models.ImageField(upload_to='users/', verbose_name='аватар', **NULLABLE)
+    avatar = models.ImageField(upload_to='user_pictures/', verbose_name='аватар', **NULLABLE)
     verification_code = models.CharField(max_length=10, verbose_name='код верификации', **NULLABLE)
 
     USERNAME_FIELD = "email"
